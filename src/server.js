1dbc646870c55cc65e471180f76f847a4c34ec86
Mohon Saha <practicewithmohon@gmail.com>
2025-07-30 14:01:00 +0600

    handle the server exist issue

--- src/server.js
+++ src/server.js
@@ -1,70 +1,54 @@
-const bodyParser = require('body-parser');
 const app = require('./app');
-const configs = require('../src/config');
+const configs = require('./config');
 require("./config/sequelize");
 const db = require('./config/sequelize');
-
-// const environment = configs.nodeEnv
-// const PORT = process.env.SERVER_PORT || 1001;
-
 
 const PORT = configs.serverPort || 1001;
 const environment = configs.nodeEnv || 'development';
 
-
-// Middleware
-app.use(bodyParser.json());
-
-
-
-
-// Error handling for unhandled promise rejections and uncaught exceptions
-
-process.on('exit', (code) => {
-  console.log(`Process exit event with code: ${code}`);
-});
-
-process.on('SIGINT', () => {
-  console.log('SIGINT received, shutting down...');
-  process.exit(0);
-});
-
-process.on('SIGTERM', () => {
-  console.log('SIGTERM received, shutting down...');
-  process.exit(0);
-});
-
 app.listen(PORT, async () => {
   try {
     console.log(`Server is alive on PORT:${PORT} in ${environment} environment`);
-    // Uncomment and wrap with try-catch to catch sync errors
-    // await db.User.sync({ alter: true });
-
+     // await db.User.sync({ alter: true });
   } catch (err) {
     console.error("❌ Error during startup:", err);
     process.exit(1);
   }
 });
 
-// app.listen(PORT, () => {
-//   console.log(`Server is alive on PORT:${PORT} in ${environment} environment`);
-// });
+
+process.on("exit", (code) => {
+  console.log(`Process exit event with code: ${code}`);
+});
+
+
+process.on("SIGINT", async () => {
+  console.log('SIGINT received, shutting down...');
+  
+  try {
+    await db.sequelize.close();
+    console.log('Database connection closed.');
+  } catch (error) {
+    console.error('Error while closing DB connection:', error);
+  }
+  
+  process.exit(0);
+});
+
+process.on("SIGTERM", () => {
+  console.log('SIGTERM received, shutting down...');
+  process.exit(0);
+});
 
 
 process.on("unhandledRejection", (error) => {
   console.log("🔴 UNHANDLED REJECTION! Server shutting down...");
   console.log(error);
-  app.on("close", () => {
-    process.exit(1);
-  });
+  process.exit(1);
 });
 
-// Error handling for uncaught exceptions
 process.on("uncaughtException", (error) => {
   console.log("🔴 UNCAUGHT EXCEPTION! Server shutting down...");
   console.log(error);
-  app.on("close", () => {
-    process.exit(1);
-  });
+  process.exit(1);
 });
-
